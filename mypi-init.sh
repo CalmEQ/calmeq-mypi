d86c296acb7e3c23c448a4abb08824c3f63efe8a
Jay D <jayfdermody@gmail.com>
2015-05-04 07:46:15 -0700

    install crtab

--- mypi-init.sh
+++ mypi-init.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-
+CALMEQ_DIR=/opt/calmeq-mypi/bin/*
 # this runs on boot (or after fresh download) to ensure the raspberry is properly configured
 
 # ensure bin directory is executable
-chmod a+x /opt/calmeq-mypi/bin/*
+echo "make bin directory executable"
+chmod a+x $CALMEQ_DIR/bin/*
+
+# setup crontab
+echo "setup crontab for pi"
+crontab -u pi $CALMEQ_DIR/pi.crontab
 
 # done!
 echo "Setup Complete"
