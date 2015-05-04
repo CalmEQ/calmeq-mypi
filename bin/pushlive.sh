2fd07675bf79d489f101531defe10e973e9285b8
Jay D <jayfdermody@gmail.com>
2015-05-04 08:00:14 -0700

    pull from id file

--- bin/pushlive.sh
+++ bin/pushlive.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 # Script to update the website
+PYDIR=/opt/calmeq-mypi/python
 MAC=$( cat /sys/class/net/eth0/address )
-ID=12
 DELAY=60
-SITE=http://calmeq-devices-alpharigel.c9.io/pies/$ID 
+
+IDFILE=/etc/calmeq-device-id
+if [ -f $IDFILE ]; then
+    ID=$( cat $IDFILE )
+else
+    echo "No ID File found at $IDFILE"
+    ID=""
+fi
+
 amixer -D hw:1 sset Mic Capture Volume 40
+SITE=http://calmeq-devices-alpharigel.c9.io/pies/$ID 
 
 while true; do 
     LAT=$( tail -n 30 gpstrack.xml  | grep '<trkpt' | tail -n 1 | sed 's:.*lat="\([-0-9.]*\)".*:\1:g' )
     LON=$( tail -n 30 gpstrack.xml  | grep '<trkpt' | tail -n 1 | sed 's:.*lon="\([-0-9.]*\)".*:\1:g' )
     NOW=$( date )
-    DBLVL=$( arecord -d 1 -D hw:1 -f S16_LE -r 44100 -t raw | python rawaudio2rms.py )
+    DBLVL=$( arecord -d 1 -D hw:1 -f S16_LE -r 44100 -t raw | python $PYDIR/rawaudio2rms.py )
 
     curl -X PATCH -d "py[lat]=$LAT" -d "py[identifier]=$MAC"  -d "py[lon]=$LON" \
 -d "py[dblvl]=$DBLVL" -d "py[devicetime]=$NOW"  $SITE \
