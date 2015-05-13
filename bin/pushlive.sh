8585e48a7ee27a5784849f036d7a468e4bf045a7
Jay <jayfdermody@gmail.com>
2015-05-13 07:37:30 +0000

    rohan suggested changes

--- bin/pushlive.sh
+++ bin/pushlive.sh
@@ -18,8 +18,8 @@ amixer -D hw:1 sset Mic Capture Volume 40
 SITE=http://calmeq-devices.herokuapp.com/pies/$ID/readings
 
 while true; do 
-    LAT=$( tail -n 30 gpstrack.xml  | grep '<trkpt' | tail -n 1 | sed 's:.*lat="\([-0-9.]*\)".*:\1:g' )
-    LON=$( tail -n 30 gpstrack.xml  | grep '<trkpt' | tail -n 1 | sed 's:.*lon="\([-0-9.]*\)".*:\1:g' )
+    LAT=$( tail -n 30 ~/gpstrack.xml  | grep '<trkpt' | tail -n 1 | sed 's:.*lat="\([-0-9.]*\)".*:\1:g' )
+    LON=$( tail -n 30 ~/gpstrack.xml  | grep '<trkpt' | tail -n 1 | sed 's:.*lon="\([-0-9.]*\)".*:\1:g' )
     NOW=$( date )
     DBLVL=$( arecord -d 1 -D hw:1 -f S16_LE -r 44100 -t raw | python $PYDIR/rawaudio2rms.py )
 
