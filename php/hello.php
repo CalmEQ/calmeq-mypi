3ec57c950c480fce3623bd6dc69a18b7a2fe626e
Jay D <jayfdermody@gmail.com>
2015-05-16 22:13:30 +0000

    add webserver stuff

---
+++ php/hello.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>My Raspberry Pi</title>
+    </head>
+    
+    <body>
+        Hello World!
+        <?php echo "Hello World moar" ?>
+    </body>
+    
+</html>
---
+++ php/wifi.php
@@ -0,0 +1,20 @@
+<!-- 
+This file displays and configures the wifi for a raspberry pi
+It relies on php for simple scripting, and html
+It is designed to be shown if the raspberry is unable to connect to one of 
+its saved networks, and had to create its own wifi network
+Then a simple phone app can pull up this page and configure the wifi
+
+for development we're going to fake some of the 
+-->
+
+<html>
+    <head>
+        <title>Wifi Configuration</title>
+    </head>
+    
+    
+    
+    
+    
+</html>
