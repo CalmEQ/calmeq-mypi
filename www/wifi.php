7e9a22b0e825d1136643ff3bee18d18ca7095bef
Jay Dermody <jayfdermody@gmail.com>
2015-05-17 22:46:40 +0000

    upates

--- www/wifi.php
+++ www/wifi.php
@@ -14,15 +14,16 @@ for development we're going to fake some of the
     </head>
     
     <body>
-        <h1>Wifi Configuration</h1>
-        <h2>Current Status: </h2>
-        <?php system('ifconfig wlan0'); echo "stuff"; ?>
-        <h2>Saved Networks</h2>
-        <table>
-            <tr>
-                <td>SSID</td><td>Passcode</td>
+      <h1>Wifi Configuration</h1>
+      <p>This page allows you to see and set the wifi configuration for the raspberry pi</p>
+      <h2>Current Status: </h2>
+      <pre><?php exec('/sbin/ifconfig wlan0', $ipinfo); echo implode("<br>", $ipinfo) ?></pre>
+      <h2>Saved Networks</h2>
+      <table>
+        <tr>
+          <td>SSID</td><td>Passcode</td>
             </tr>
-        </table>
+      </table>
     </body>
     
     
