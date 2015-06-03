fe101a0ddcb5cb0e4f8254846978932247eb3292
Jay Dermody <jayfdermody@gmail.com>
2015-06-03 05:26:26 +0000

    add webserver to setup

--- mypi-init.sh
+++ mypi-init.sh
@@ -4,19 +4,24 @@ CALMEQ_DIR=/opt/calmeq-mypi
 # this runs on boot (or after fresh download) to ensure the raspberry is properly configured
 
 # ensure bin directory is executable
-echo "make bin directory executable"
+echo ""
+echo "--- make bin directory executable ---"
 chmod a+x $CALMEQ_DIR/bin/*
 
-sudo apt-get install -y gpsd gpsd-clients
-
 #start gpsd
+echo ""
+echo "--- update and start gpsd ---"
+sudo apt-get install -y gpsd gpsd-clients
 gpsd /dev/ttyUSB0
 
 # setup crontab
-echo "setup crontab for pi"
+echo ""
+echo "--- setup crontab for pi ---"
 crontab -u pi $CALMEQ_DIR/pi.crontab
 
 # make sure we can ssh from middle machine
+echo ""
+echo "--- setup ssh tunnel ---"
 grep "$(cat $CALMEQ_DIR/tunnel.rsa.pub)" /home/pi/.ssh/authorized_keys > /dev/null
 if [[ $? -ne 0 ]]; then
     if [ ! -d /home/pi/.ssh ]; then
@@ -41,6 +46,16 @@ fi
 # scp calmeq-tunnel.pem.txt pi@mypi.local:~/.ssh/calmeq-tunnel.pem
 # ssh pi@mypi.local  "chmod 400 ~/.ssh/calmeq-tunnel.pem"
 
+
+# setup the webserver
+echo ""
+echo "--- update and setup webserver ---"
+sudo apt-get install apache2
+cp apache2/default /etc/apache2/sites-available/default
+sudo apachectl -k start  # it should be up by default, but just to make sure
+
+
 # done!
-echo "Setup Complete"
+echo ""
+echo "--- Setup Complete ---"
 
