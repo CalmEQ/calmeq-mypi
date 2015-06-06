b562af912836b314c77d5af2ac478f55798068c3
root <root@fourpi.(none)>
2015-06-06 00:42:20 +0000

    update the formatting to use bootstrap

--- www/index.php
+++ www/index.php
@@ -1,14 +1,41 @@
 <html>
   <head>
-    <title>CalmEQ RaspberryPi</title>
-  </head>
-  <body>
-    <h2>System Status</h1><br>
-    <?php system('uname -a') ?><br>
-    <h2>Wifi Configuration</h2>
-    <a href="wifi.php">Wifi Configuration</a><br>
-    <br>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
+    <title>CalmEQ RaspberryPi</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="theme.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+    <div class="container theme-base" role="main">
+
+      <h1>CalmEQ Raspberry System</h1>
+      <p>Welcome to the CalmEQ Raspberry Configuration System</p>
+      
+      <h2>System Status</h2>
+      <p><pre><?php system('uname -a') ?></pre></p>
+      <br>
+      <a href="wifi.php"><button class="btn btn-lg btn-primary">Wifi Configuration</button></a><br>
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+
+      <!-- Latest compiled and minified JavaScript -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+    </div>
   </body>
+
 </html>
 
--- www/setwifi.php
+++ www/setwifi.php
@@ -1,17 +1,43 @@
 <html>
   <head>
-    <title>Setting Wifi Configuration</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <title>Adding Wifi Configuration</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="theme.css" rel="stylesheet">
+
   </head>
 
   <body>
-    <h1>Setting Wifi Configuration</h1>
-    <p>This sets the wifi based on the previously submited form: </p>
-    <h2>Result of set:</h2>
-    <?php exec('/opt/calmeq-mypi/bin/setwifi.sh '. $_POST['ssid'] 
-               .' '. $_POST['passcode'] .' '. $_POST['keymgmt'], $outtext); echo implode("<br>", $outtext) ?>
+    <div class="container theme-base" role="main">
+
+    <h1>Adding Wifi Configuration</h1>
+    <p>This adds the wifi based on the previously submited form: </p>
+    <h3>Result of add</h3>
+    <pre><?php exec('/opt/calmeq-mypi/bin/setwifi.sh '. $_POST['ssid'] 
+               .' '. $_POST['passcode'] .' '. $_POST['keymgmt'], $outtext); echo implode("<br>", $outtext) ?></pre>
     <br>
     <br>
-    <a href="wifi.php">Back</a>
+    <a href="wifi.php"><button class="btn btn-primary">Back</button></a>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+    </div>
   </body>
 
 
