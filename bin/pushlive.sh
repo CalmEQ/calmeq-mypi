35d8478f8f907db63998d78124d63f3bb2cd167c
cprohan <cprohan@users.noreply.github.com>
2015-06-18 17:51:48 -0500

    Updated pushlive to retrieve the id of the pie by first doing a POST to the /pies path with the MAC as the identifier. Now the /etc/calmeq-device-id should not be required

--- bin/pushlive.sh
+++ bin/pushlive.sh
@@ -15,22 +15,27 @@ PYDIR=/opt/calmeq-mypi/python
 MAC=$( cat /sys/class/net/eth0/address )
 DELAY=60
 
-IDFILE=/etc/calmeq-device-id
-if [ -f $IDFILE ]; then
-    ID=$( cat $IDFILE )
-else
-    echo "No ID File found at $IDFILE"
-    ID=""
-fi
+#IDFILE=/etc/calmeq-device-id
+#if [ -f $IDFILE ]; then
+#    ID=$( cat $IDFILE )
+#else
+#    echo "No ID File found at $IDFILE"
+#    ID=""
+#fi
 
 amixer -D hw:1 sset Mic Capture Volume 40
 
 if [[ ! $CALMEQ_DEVICE_SERVER ]]; then
     CALMEQ_DEVICE_SERVER="http://calmeq-devices.herokuapp.com"
 fi
-SITE=$CALMEQ_DEVICE_SERVER/pies/$ID/readings
 
+SITE=$CALMEQ_DEVICE_SERVER/pies/$ID/readings
 TRUEVAR=1
+ID=$( curl -X POST --data identifier=$MAC $CALMEQ_DEVICE_SERVER/pies )
+if [ $ID -eq 0 ]; then
+    TRUEVAR=0
+fi
+    
 while [ $TRUEVAR -eq 1 ]; do 
     LAT=$( tail -n 30 ~/gpstrack.xml  | grep '<trkpt' | tail -n 1 | sed 's:.*lat="\([-0-9.]*\)".*:\1:g' )
     LON=$( tail -n 30 ~/gpstrack.xml  | grep '<trkpt' | tail -n 1 | sed 's:.*lon="\([-0-9.]*\)".*:\1:g' )
